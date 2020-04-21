8e15496038602d33482578dac2dc2b7a515b182c
Amneriis <antoine.lalloue@ynov.com>
2020-04-21 17:57:04 +0200

    fix(front)Modif url paterns and static

--- Dahlias/Dahlias/urls.py
+++ Dahlias/Dahlias/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from . import views
 
-app_name = 'main'
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
-    path('accueil/', views.accueil, name='accueil'),
-    path('equipes/', views.equipes, name='equipes'),
-    path('admin/', admin.site.urls, name='admin'),
+    path('', include("main.urls"), name='main'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- Dahlias/main/urls.py
+++ Dahlias/main/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.accueil, name='accueil'),
-    path('accueil/', views.accueil, name='accueil'),
     path('equipes/', views.equipes, name='equipes'),
     path('admin/', admin.site.urls, name='admin'),
 ]
